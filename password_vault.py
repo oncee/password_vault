ce8dcf791c41f95476c1d30cb5a73274193eb2e0
Bill Gardner (oncee) <bill.gardner@gmail.com>
2022-12-23 22:11:37 -0500

    Update password_vault.py

--- password_vault.py
+++ password_vault.py
@@ -12,31 +12,33 @@ def create_seed_phrase():
   return seed_phrase.strip()
 
 def create_password_vault():
-  # Prompt the user to create a seed phrase
+  # Prompt the user to create a seed phrase and a passphrase
   print("Welcome to the password vault!")
-  print("To create a new password vault, you will need to set up a 24-word seed phrase.")
+  print("To create a new password vault, you will need to set up a 24-word seed phrase and a passphrase.")
   print("Please write down the following seed phrase and store it in a secure location:")
   seed_phrase = create_seed_phrase()
   print(seed_phrase)
-  # Hash the seed phrase to use as the password for the password vault
-  password = hashlib.sha256(seed_phrase.encode()).hexdigest()
+  passphrase = getpass.getpass("Enter a passphrase: ")
+  # Hash the seed phrase and passphrase to use as the password for the password vault
+  password = hashlib.sha256((seed_phrase + passphrase).encode()).hexdigest()
   # Create a new password vault file
   with open("password_vault.txt", "w") as f:
     f.write(password)
 
 def login_to_password_vault():
-  # Prompt the user to enter the seed phrase
-  print("Enter your seed phrase to access the password vault:")
-  seed_phrase = getpass.getpass()
-  # Hash the seed phrase to use as the password for the password vault
-  password = hashlib.sha256(seed_phrase.encode()).hexdigest()
+  # Prompt the user to enter the seed phrase and passphrase
+  print("Enter your seed phrase and passphrase to access the password vault:")
+  seed_phrase = getpass.getpass("Seed phrase: ")
+  passphrase = getpass.getpass("Passphrase: ")
+  # Hash the seed phrase and passphrase to use as the password for the password vault
+  password = hashlib.sha256((seed_phrase + passphrase).encode()).hexdigest()
   # Open the password vault file and check the password
   with open("password_vault.txt", "r") as f:
     stored_password = f.read()
   if password == stored_password:
     print("Access granted. Welcome to the password vault.")
   else:
-    print("Access denied. Incorrect seed phrase.")
+    print("Access denied. Incorrect seed phrase or passphrase.")
 
 def add_password(website, username, password):
   # Encrypt the password using the password vault password
@@ -51,10 +53,4 @@ def add_password(website, username, password):
 
 def get_password(website, username):
   # Open the password vault file and search for the website and username
-  with open("password_vault.txt", "r") as f:
-    lines = f.readlines()
-  for line in lines:
-    data = pickle.loads(base64.b64decode(line))
-    if data[0] == website and data[1] == username:
-      # Decrypt the password using the password vault password
-      password_vault_password = lines[0
+  with open("password_vault.txt", "r") as
