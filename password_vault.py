b87a4c9ee752445b8106cb8cafb26a923134966f
Bill Gardner (oncee) <bill.gardner@gmail.com>
2022-12-24 21:19:22 -0500

    Update password_vault.py

--- password_vault.py
+++ password_vault.py
@@ -1,7 +1,11 @@
 import hashlib
-import getpass
-import os
 import base64
+import pickle
+import os
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.hashers import check_password
 
 def create_seed_phrase():
   # Generate a random 24-word seed phrase
@@ -11,46 +15,53 @@ def create_seed_phrase():
     seed_phrase += words[random.randint(0, len(words)-1)] + " "
   return seed_phrase.strip()
 
-def create_password_vault():
-  # Prompt the user to create a seed phrase and a passphrase
-  print("Welcome to the password vault!")
-  print("To create a new password vault, you will need to set up a 24-word seed phrase and a passphrase.")
-  print("Please write down the following seed phrase and store it in a secure location:")
-  seed_phrase = create_seed_phrase()
-  print(seed_phrase)
-  passphrase = getpass.getpass("Enter a passphrase: ")
-  # Hash the seed phrase and passphrase to use as the password for the password vault
-  password = hashlib.sha256((seed_phrase + passphrase).encode()).hexdigest()
-  # Create a new password vault file
-  with open("password_vault.txt", "w") as f:
-    f.write(password)
-
-def login_to_password_vault():
-  # Prompt the user to enter the seed phrase and passphrase
-  print("Enter your seed phrase and passphrase to access the password vault:")
-  seed_phrase = getpass.getpass("Seed phrase: ")
-  passphrase = getpass.getpass("Passphrase: ")
-  # Hash the seed phrase and passphrase to use as the password for the password vault
-  password = hashlib.sha256((seed_phrase + passphrase).encode()).hexdigest()
-  # Open the password vault file and check the password
-  with open("password_vault.txt", "r") as f:
-    stored_password = f.read()
-  if password == stored_password:
-    print("Access granted. Welcome to the password vault.")
+def create_password_vault(request):
+  if request.method == "POST":
+    # Hash the seed phrase and passphrase to use as the password for the password vault
+    seed_phrase = request.POST.get("seed_phrase")
+    passphrase = request.POST.get("passphrase")
+    password = hashlib.sha256((seed_phrase + passphrase).encode()).hexdigest()
+    # Create a new password vault file
+    with open("password_vault.txt", "w") as f:
+      f.write(password)
+    return redirect("login")
   else:
-    print("Access denied. Incorrect seed phrase or passphrase.")
+    # Prompt the user to create a seed phrase and a passphrase
+    context = {
+      "seed_phrase": create_seed_phrase()
+    }
+    return render(request, "create_password_vault.html", context)
 
-def add_password(website, username, password):
-  # Encrypt the password using the password vault password
-  with open("password_vault.txt", "r") as f:
-    password_vault_password = f.read()
-  cipher = AES.new(password_vault_password, AES.MODE_EAX)
-  ciphertext, tag = cipher.encrypt_and_digest(password.encode())
-  # Store the encrypted password in the password vault file
-  with open("password_vault.txt", "a") as f:
-    data = [website, username, ciphertext, tag]
-    f.write(base64.b64encode(pickle.dumps(data)).decode() + "\n")
+def login(request):
+  if request.method == "POST":
+    # Hash the seed phrase and passphrase to use as the password for the password vault
+    seed_phrase = request.POST.get("seed_phrase")
+    passphrase = request.POST.get("passphrase")
+    password = hashlib.sha256((seed_phrase + passphrase).encode()).hexdigest()
+    # Open the password vault file and check the password
+    with open("password_vault.txt", "r") as f:
+      stored_password = f.read()
+    if check_password(password, stored_password):
+      request.session["logged_in"] = True
+      return redirect("home")
+    else:
+      return HttpResponse("Incorrect seed phrase or passphrase.")
+  else:
+    return render(request, "login.html")
 
-def get_password(website, username):
-  # Open the password vault file and search for the website and username
-  with open("password_vault.txt", "r") as
+def home(request):
+  if not request.session.get("logged_in"):
+    return redirect("login")
+  return render(request, "home.html")
+
+def add_password(request):
+  if not request.session.get("logged_in"):
+    return redirect("login")
+  if request.method == "POST":
+    # Encrypt the password using the password vault password
+    with open("password_vault.txt", "r") as f:
+      password_vault_password = f.read()
+    cipher = AES.new(password_vault_password, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(request.POST.get("password").encode())
+    #
+
